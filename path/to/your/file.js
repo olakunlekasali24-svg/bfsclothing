7690b26cd693c74d8d6aef69fe746240ba77cee5
olakunlekasali24-svg <olakunlekasali24@gmail.com>
2026-04-08 00:24:47 +0100

    Fix JavaScript Syntax Errors and Typos

---
+++ path/to/your/file.js
@@ -0,0 +1,9 @@
+// Fixing the duplications and syntax errors in JavaScript
+
+// Removed duplicate HTML login modal markup
+    
+// fixing height typo
+height: 60px;
+
+// Fixed the missing quote
+class="product-image"
